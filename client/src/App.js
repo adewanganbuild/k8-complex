b5521c45fe95ab07a61c7d1b733690f13746e987
Ashish Dewangan <ashishdewangan.learn@gmail.com>
2023-08-16 18:42:16 +0530

    Add architecture for kubernetes deployment

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function App() {
             Learn React
           </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">My Other Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
